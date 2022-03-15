fb25481bca0d19c8c2e8eb857509e7c2470fc1d7
f0xt0xcity <manueloctsi@gmail.com>
2022-03-15 11:38:56 -0600

    Imagenes con alt

--- pages/about.jsx
+++ pages/about.jsx
@@ -13,17 +13,17 @@ const about = () => {
             <div className='text-center'>
               <div className='row'>
                 <div className='col-6 col-sm-4'>
-                  <Image src={'/img/certificadoCloud.png'} layout='responsive' width={ 480 } height={ 480 }/>
+                  <Image alt='Imagen del certificado de cloud computing' src={'/img/certificadoCloud.png'} layout='responsive' width={ 480 } height={ 480 }/>
                 </div>
                 <div className='col-6 col-sm-4'>
-                  <Image src={'/img/certificadoBussines.png'} layout='responsive' width={ 480 } height={ 480 }/>
+                  <Image alt='Imagen del certificado de Google bussines' src={'/img/certificadoBussines.png'} layout='responsive' width={ 480 } height={ 480 }/>
                 </div>
                 <div className='col-6 col-sm-4'>
-                  <Image src={'/img/certificadoMkt.png'} layout='responsive' width={ 480 } height={ 480 }/>
+                  <Image alt='Imagen del certificado de marketing' src={'/img/certificadoMkt.png'} layout='responsive' width={ 480 } height={ 480 }/>
                 </div>
               </div>
               <Link href={'https://drive.google.com/drive/folders/1ARwKxoeIQSiFX4KjuS07I073jRiqWBpO?usp=sharing'}>
-                <a className='btn btn-primary mt-5  mb-5'>Ver todos los certificados</a>
+                <a className='btn btn-primary mt-5  mb-5'>Ver certificados en pdf</a>
               </Link>
             </div>
         </div>
